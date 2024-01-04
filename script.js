1783e9fa5fd1b2c965819ea7deed36b84dc4053d
benlevinas <150365734+benlevinas@users.noreply.github.com>
2024-01-05 01:31:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const textContainer = document.getElementById("textContainer");
 const bottomLinks = document.getElementById("bottomLinks");
 const texts = [
-  'Ben Levinas is an industrial designer building narratives, typologies, and ecosystems for a new future.',
+  'Ben Levinas is an industrial designer \nbuilding narratives, sensibilities, and \ntypologies for a new future.',
   '"We become what we behold. We shape \nour tools and then our tools shape us."\n- Father John Culkin'
 ];
 let currentIndex = 0;
