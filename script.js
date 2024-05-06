cebb9fdc94e7cb63f004597bc854ca6a8ecb89e7
benlevinas <150365734+benlevinas@users.noreply.github.com>
2024-05-06 16:59:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const textContainer = document.getElementById("textContainer");
+const linkcontainer = document.getElementById("linkcontainer");
 const bottomLinks = document.getElementById("bottomLinks");
 const texts = [
-  'Ben Levinas is an industrial designer \nbuilding narratives, sensibilities, \nand typologies for a new future.',
+  'Ben Levinas is an industrial designer building \nnarratives, sensibilities, and typologies for a new future.',
   '"We become what we behold. We shape \nour tools and then our tools shape us."\n- Father John Culkin'
 ];
 let currentIndex = 0;
@@ -9,55 +10,14 @@ let currentIndex = 0;
 function fadeInElements() {
   textContainer.style.opacity = 1;
   bottomLinks.style.opacity = 1;
+  linkcontainer.style.opacity = 1;
 }
 
 function updateText() {
   textContainer.innerHTML = texts[currentIndex]; // Use innerHTML to interpret HTML tags
   textContainer.style.animation = "fadeIn 2s ease-in-out forwards";
+  linkcontainer.style.animation = "fadeIn 2s ease-in-out 0.5s forwards"
   bottomLinks.style.animation = "fadeIn 2s ease-in-out 2s forwards";
-  setTimeout(function () {
-    textContainer.style.animation = "fadeOut 2s ease-in-out forwards";
-    currentIndex = (currentIndex + 1) % texts.length;
-    setTimeout(() => {
-      updateText();
-      fadeInElements();
-    }, 2000);
-  }, 8000);
 }
 
 setTimeout(updateText, 1000);
-
-// Check if the user prefers dark mode
-const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-const body = document.body;
-
-function updateTheme() {
-    if (darkModeMediaQuery.matches) {
-        // User prefers dark mode
-        body.classList.add('dark-mode');
-        updateDarkModeLinks(true);
-    } else {
-        // User prefers light mode
-        body.classList.remove('dark-mode');
-        updateDarkModeLinks(false);
-    }
-}
-
-function updateDarkModeLinks(isDarkMode) {
-    const links = document.querySelectorAll('.bottom-links a');
-
-    links.forEach(link => {
-        if (isDarkMode) {
-            link.classList.add('dark-mode');
-        } else {
-            link.classList.remove('dark-mode');
-        }
-    });
-}
-
-// Initial theme update
-updateTheme();
-
-// Listen for changes in the color scheme preference
-darkModeMediaQuery.addEventListener('change', updateTheme);
-
