978701577ebb6c5d65cfcf648c6e6a79189bc118
benlevinas <hello@benlevinas.com>
2025-02-10 11:48:44 +0100

    Wrapping Update

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const textContainer = document.getElementById("textContainer");
 const linkcontainer = document.getElementById("linkcontainer");
 const bottomLinks = document.getElementById("bottomLinks");
-const texts = [
-  'Ben Levinas is an industrial designer building \nnarratives, sensibilities, and typologies for the future.',
-  '"We become what we behold. We shape \nour tools and then our tools shape us."\n- Father John Culkin'
+const texts = [   
+  'Ben Levinas is an industrial designer building narratives, sensibilities, and typologies for the future.',   
+  '"We become what we behold. We shape our tools and then our tools shape us."\n- Father John Culkin' 
 ];
 let currentIndex = 0;
 
@@ -14,9 +14,10 @@ function fadeInElements() {
 }
 
 function updateText() {
-  textContainer.innerHTML = texts[currentIndex]; // Use innerHTML to interpret HTML tags
+  // Replace \n with actual line breaks for better control
+  textContainer.textContent = texts[currentIndex].replace(/\\n/g, '\n');
   textContainer.style.animation = "fadeIn 2s ease-in-out forwards";
-  linkcontainer.style.animation = "fadeIn 2s ease-in-out 0.5s forwards"
+  linkcontainer.style.animation = "fadeIn 2s ease-in-out 0.5s forwards";
   bottomLinks.style.animation = "fadeIn 2s ease-in-out 2s forwards";
 }
 
